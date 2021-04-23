5c636f52f689be3dba97710ae35fc8d218f898b6
Moumen Hallak <moumen95@gmail.com>
2021-04-23 20:55:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ function greet(timeString){
 /* Write your implementation of displayMessage() */
 function displayMessage(msg){
 let content = document.getElementById("greeting").innerText
-      expect(content).to.equal(testContent)
+      
 }
