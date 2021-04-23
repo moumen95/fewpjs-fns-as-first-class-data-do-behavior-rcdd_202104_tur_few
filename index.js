9d60ab73b96da64a78f0e3ca00c8abdba88ae82a
Moumen Hallak <moumen95@gmail.com>
2021-04-23 20:28:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function handleClick(e) {
 
 function greet(timeString){
   if (timeString < "12"){
-  // return 'Good Morning';
-  console.log('Good Morning');
+  return 'Good Morning';
+  // console.log('Good Morning');
   } else if (timeString > "12" && timeString < "17"){
-  // return 'Good Afternoon';
-  console.log('Good Afternoon')
+  return 'Good Afternoon';
+  // console.log('Good Afternoon')
 
   } else
-    // return 'Good Evening';
-    console.log('Good Evening')
+    return 'Good Evening';
+    // console.log('Good Evening')
   
 }
 
